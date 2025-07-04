302303db35295e6c0e61847f84c52e1bcff1f24f
Roland Groen <roland@headease.nl>
2025-07-04 09:56:49 +0200

    Revert "Remove validation step from run-scripts.sh"
    This reverts commit 18df16fdf5bcac5eadb6ddac979b6628bfd5615b.

--- run-scripts.sh
+++ run-scripts.sh
@@ -5,4 +5,7 @@ chmod +x ./scripts/*.sh
 fhir install nictiz.fhir.nl.r4.zib2020 0.11.0-beta.1
 fhir install nictiz.fhir.nl.r4.nl-core 0.11.0-beta.1
 ./scripts/make-packages.sh
+./scripts/validate-examples.sh
+
+
 
