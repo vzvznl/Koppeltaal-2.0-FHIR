ad155f207726a7da28ba14c55aefe27e1d997178
Roland Groen <roland@headease.nl>
2025-07-04 09:50:28 +0200

    Revert "MAINTENANCE refactor scripts and Dockerfile for improved FHIR package handling; add new CodeSystem resource and package configuration."
    This reverts commit 7ca4f1550ba89e650fcd361854172c0791698d1c.

--- run-scripts.sh
+++ run-scripts.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 export PATH=$PATH:$HOME/.dotnet/tools
 fhir login email=$FHIR_EMAIL password=$FHIR_PASSWORD
+chmod +x ./scripts/*.sh
 fhir install nictiz.fhir.nl.r4.zib2020 0.11.0-beta.1
 fhir install nictiz.fhir.nl.r4.nl-core 0.11.0-beta.1
-fhir pack
+./scripts/make-packages.sh
 
